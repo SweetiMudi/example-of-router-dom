3f7f47fca8bc0717a1f7f8ce0a6bcc5c9b4b1767
SWEETI MUDI <118963788+SweetiMudi@users.noreply.github.com>
2022-12-28 11:18:11 +0530

    Update Menu.jsx

--- Menu.jsx
+++ Menu.jsx
@@ -1,4 +1,21 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom'
+
+const Menu = () => {
+  
+    return(
+     <>
+        <NavLink exact activeClassName="active_class" to="/">ABOUT US</NavLink> <br/>
+        <NavLink exact activeClassName="active_class" to="/contact">CONTACT</NavLink> <br/>
+     </>
+    )
+    };
+export default Menu;
+
+
+
+/* it will reload the page
+import React from 'react';
 
 const Menu = () => {
   
@@ -14,3 +31,4 @@ const Menu = () => {
     )
     };
 export default Menu;
+*/
