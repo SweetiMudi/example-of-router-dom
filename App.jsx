200c3358ad88841c0698bf751e1466484cceae41
SWEETI MUDI <118963788+SweetiMudi@users.noreply.github.com>
2022-12-28 10:49:56 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -3,12 +3,13 @@ import { Route , Routes} from 'react-router-dom';
 import About from './About';
 import Contact from './Contact';
 import Error from './Error';
-
+import Menu from './Menu';
 
 const App = () => {
   
     return(
      <>
+      <Menu/>
       <Routes>
        <Route exact path ='/' element={<About/>} />
        <Route path ='/contact' element={<Contact/>} />
