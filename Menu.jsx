c3fe5764b89b7b2df670642a09a88096f1561018
SWEETI MUDI <118963788+SweetiMudi@users.noreply.github.com>
2022-12-28 10:48:02 +0530

    Create Menu.jsx

---
+++ Menu.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Menu = () => {
+  
+    return(
+     <>
+     <nav class="nav flex-column">
+         <a class="nav-link active" aria-current="page" href="/">About</a><br/>
+         <a class="nav-link" href="/contact">Contact</a><br/>
+         <a class="nav-link" href="/*"></a>
+        
+     </nav>
+     </>
+    )
+    };
+export default Menu;
